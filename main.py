a0bc17ddf8ada12fd0c1ad8c59caf90f4df1427e
OBlTORIN <obitocreations01@gmail.com>
2026-05-06 22:20:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 import yfinance as yf
 import pandas as pd
 
@@ -15,7 +16,7 @@ app.add_middleware(
 
 @app.get("/")
 def home():
-    return {"status": "working"}
+    return FileResponse("index.html")
 
 @app.get("/signal")
 def signal(balance: float = 100):
@@ -23,7 +24,7 @@ def signal(balance: float = 100):
     df = yf.download("GC=F", period="5d", interval="5m")
 
     if df.empty:
-        return {"error": "No Data"}
+        return {"error":"No Data"}
 
     df = df.dropna()
 
@@ -32,12 +33,8 @@ def signal(balance: float = 100):
 
     close = df["Close"]
 
-    # EMA
-
     ema9 = close.ewm(span=9).mean()
     ema21 = close.ewm(span=21).mean()
-
-    # RSI
 
     delta = close.diff()
 
@@ -51,19 +48,17 @@ def signal(balance: float = 100):
 
     rsi = 100 - (100 / (1 + rs))
 
-    current_price = round(float(close.iloc[-1]), 1)
+    current_price = round(float(close.iloc[-1]),1)
 
     current_rsi = float(rsi.iloc[-1])
-
-    # SIGNAL LOGIC
 
     if ema9.iloc[-1] > ema21.iloc[-1] and current_rsi > 50:
 
         signal = "BUY"
 
-        sl = round(current_price - 10, 1)
+        sl = round(current_price - 10,1)
 
-        tp = round(current_price + 20, 1)
+        tp = round(current_price + 20,1)
 
         confidence = 88
 
@@ -71,33 +66,31 @@ def signal(balance: float = 100):
 
         signal = "SELL"
 
-        sl = round(current_price + 10, 1)
+        sl = round(current_price + 10,1)
 
-        tp = round(current_price - 20, 1)
+        tp = round(current_price - 20,1)
 
         confidence = 86
-
-    # RISK MANAGEMENT
 
     risk_amount = balance * 0.02
 
     sl_distance = abs(current_price - sl)
 
-    lot_size = round(risk_amount / (sl_distance * 10), 2)
+    lot_size = round(risk_amount / (sl_distance * 10),2)
 
-    tp_profit = round(abs(tp - current_price) * lot_size * 10, 2)
+    tp_profit = round(abs(tp-current_price)*lot_size*10,2)
 
-    sl_loss = round(abs(sl - current_price) * lot_size * 10, 2)
+    sl_loss = round(abs(sl-current_price)*lot_size*10,2)
 
     return {
 
-        "signal": signal,
-        "entry": current_price,
-        "sl": sl,
-        "tp": tp,
-        "lot": lot_size,
-        "profit": tp_profit,
-        "loss": sl_loss,
-        "confidence": f"{confidence}%"
+        "signal":signal,
+        "entry":current_price,
+        "sl":sl,
+        "tp":tp,
+        "lot":lot_size,
+        "profit":tp_profit,
+        "loss":sl_loss,
+        "confidence":f"{confidence}%"
 
     }
